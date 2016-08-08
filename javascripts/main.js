1b547201c18fdce482f3c14207ccd22a6fd2a9a0
GILBERT GONZALES <gilbertgonzales@GILBERTs-MacBook-Pro.local>
2016-08-08 15:29:32 -0500

    intro to js homework answers

---
+++ javascripts/main.js
@@ -0,0 +1,64 @@
+// Part 1: varibles
+var givenName;
+// Q: What does `givenName` equal right now?
+// A:null
+
+givenName = "Tim";
+// Q: What is `givenName` set to right now?
+// A: Tim
+console.log(givenName);
+
+givenName = "JD";
+// Q: What is `givenName` set to right now?
+// A: JD
+console.log (givenName);
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+// A: " Hello, how are you?"
+console.log (greeting);
+
+/*=========================================================
+     Part 2: Simple Math
+*/
+
+var high = 50;
+var low  = 10;
+console.log (high);
+console.log (low);
+//A: High displays 50 low displays 10
+
+var math = high - low;
+// Q: What is `math` set to?
+// A: 40
+console.log (high - low);
+
+math = high - "5";
+// Q: What is `math` set to?
+// A: 45
+console.log(high - "5");
+
+//Part 3: Expressions
+//
+
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 32 years old"
+// The answer shoud not be written in a comment.
+var born = 1983;
+var today = 2015;
+
+// Answers Below:
+var timsAge = today - born;
+console.log("Tim is " + timsAge + " years old .");
+
+// Store some information following in variables.
+
+var yourname = "Gilbert Gonzales";
+var instructorName = "Jason Vanderslice";
+console.log(yourname);
+console.log(instructorName);
+
+// Final Statement
+// var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+
+console.log(yourname + " is taking a class at the Iron Yard,my instructors's name is " + instructorName +".");
